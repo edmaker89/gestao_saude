4e448bd88df8c3b0f31f4c1dee7ed5c66c6d71c4
edmaker <edmaker@gmail.com>
2023-12-24 10:57:02 -0300

    feat: implement function create button_layout

---
+++ app/utils/dict_layout.py
@@ -0,0 +1,10 @@
+def button_layout(url:str, classname='', label='', icon=''):
+    
+    button_layout = {
+        'classname': classname,
+        'label': label,
+        'icon': icon,
+        'url': url
+    }
+
+    return button_layout
